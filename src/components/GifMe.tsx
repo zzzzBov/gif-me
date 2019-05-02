2087854f01ae5c36e339de4426787c4dff3e9e26
Tim Leverett <zzzzbov@gmail.com>
2019-05-02 12:15:34 -0400

    Form updates query string

--- src/components/GifMe.tsx
+++ src/components/GifMe.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 
+import { useQueryString } from '../useQueryString'
+
 import { Footer } from './Footer'
 import { Results } from './Results'
 import { Search } from './Search'
 
 export const GifMe: React.FunctionComponent = () => {
-  const [value, setValue] = React.useState('')
+  const [value, setValue] = useQueryString('q')
 
   return (
     <div className="GifMe">
